5b282937fe9a71a6addab4a17db6cdc2879b0c45
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-01-11 15:17:01 -0800

    Fix experimental script by invoking normal factor directly

--- neuralpp/experiments/normal_factor_log_prob.py
+++ neuralpp/experiments/normal_factor_log_prob.py
@@ -12,21 +12,17 @@ scale = TensorVariable("scale", 0)
 
 normal_factor = NormalFactor([value, loc, scale])
 
-
-# Note: in order for this to run we will have to fix pytorch_distribution_factor.py:
-# distribution = self.pytorch_distribution_maker(*distribution_parameters)
-prob = normal_factor.call_after_validation(
+prob = normal_factor(
     {
         value: torch.tensor(0.0),
         loc: torch.tensor(0.0),
         scale: torch.tensor(1.0),
-    },
-    assignment_values=None,  # question: what does this parameter do?
+    }
 )
 
 # note to myself: this returns the probability, not log prob
 print(prob)
 
-# compare to the log prob from torch
+# compare to the log prob from calling torch distribution directly
 log_prob = dist.Normal(torch.tensor(0.0), torch.tensor(1.0)).log_prob(torch.tensor(0.0))
 assert prob == log_prob.exp()
