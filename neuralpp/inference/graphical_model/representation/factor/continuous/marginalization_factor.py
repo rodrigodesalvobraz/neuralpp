9cd4b6ff31ec46061d23690ee6d31f4459204213
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-02-15 18:44:45 -0800

    Swap out the base class of marginalization factor and implement sum_out_variable(?)

--- neuralpp/inference/graphical_model/representation/factor/continuous/marginalization_factor.py
+++ neuralpp/inference/graphical_model/representation/factor/continuous/marginalization_factor.py
@@ -1,38 +1,26 @@
-from neuralpp.inference.graphical_model import variable
-from neuralpp.inference.graphical_model.representation.factor.continuous.continuous_factor import (
-    ContinuousFactor,
-)
 from neuralpp.inference.graphical_model.representation.factor.factor import Factor
 from neuralpp.inference.graphical_model.variable.integer_variable import (
     DiscreteVariable,
 )
 
-# TODO: make Factor class takes in the conditioning_dict and swap the base class here
-class MarginalizationFactor(ContinuousFactor):
-    def __init__(
-        self,
-        factor: ContinuousFactor,
-        marginalized_variable: DiscreteVariable,
-        conditioning_dict=None,
-    ):
+
+class MarginalizationFactor(Factor):
+    def __init__(self, factor: Factor, marginalized_variable: DiscreteVariable):
         if not isinstance(marginalized_variable, DiscreteVariable):
             raise ValueError("Only discrete variables can be marginalized.")
+        super().__init__(set(factor.variables) - {marginalized_variable})
 
-        variables = [var for var in factor.variables if var != marginalized_variable]
-        super().__init__(variables, conditioning_dict)
         self.raw_factor = factor
         self.marginalized_variable = marginalized_variable
 
     def condition_on_non_empty_dict(self, assignment_dict):
         return MarginalizationFactor(
-            self.raw_factor,
+            self.raw_factor.condition(assignment_dict),
             self.marginalized_variable,
-            self.total_conditioning_dict(assignment_dict),
         )
 
     def call_after_validation(self, assignment_dict, assignment_values):
         prob = 0.0
-        assignment_dict = self.total_conditioning_dict(assignment_dict)
         for val in self.marginalized_variable.assignments():
             full_assignment_dict = {**assignment_dict, self.marginalized_variable: val}
             prob += self.raw_factor(full_assignment_dict)
@@ -44,9 +32,11 @@ class MarginalizationFactor(ContinuousFactor):
                 f"{other} contains a variable that has been marginalized out."
             )
 
-        prod_factor = self.raw_factor * other
-        return MarginalizationFactor(
-            prod_factor,
-            self.marginalized_variable,
-            self.total_conditioning_dict(self.conditioning_dict),
-        )
+        product = self.raw_factor * other
+        return MarginalizationFactor(product, self.marginalized_variable)
+
+    def sum_out_variable(self, variable):
+        if variable == self.marginalized_variable:
+            return
+        reduced = self.raw_factor.sum_out_variable(variable)
+        return MarginalizationFactor(reduced, self.marginalized_variable)
