381cd9fad41350c6c82efcb7a337346cd3a7d1a7
Rodrigo De Salvo Braz <rodrigobraz@fb.com>
2022-03-02 10:58:37 -0800

    Improve efficiency and simplicity of table sampling code.

--- neuralpp/inference/graphical_model/representation/table/pytorch_table.py
+++ neuralpp/inference/graphical_model/representation/table/pytorch_table.py
@@ -282,7 +282,7 @@ class PyTorchTable(Table):
         batch_size_x_n_x_assignment_indices = Categorical(batch_size_x_n_x_potentials).sample()
         batch_size_x_n_x_assignments = self.non_batch_radices.representation(batch_size_x_n_x_assignment_indices)
 
-        need_to_squeeze = not self.batch or n == 1
+        need_to_squeeze = batch_size == 1 or n == 1
 
         if need_to_squeeze:
             slices_for_squeezing_batch_and_n_if_needed = [
