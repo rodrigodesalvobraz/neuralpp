9fc12c8e229962b958fdfda18e4fec98d25a99ee
Xiang Wu <hsiangwu@fb.com>
2022-06-08 13:00:31 -0700

    fixed bunch of problems according to code reviews

--- neuralpp/test/quick_tests/symoblic/solver_compare_test.py
+++ neuralpp/test/quick_tests/symoblic/solver_compare_test.py
@@ -2,16 +2,18 @@
 Test cases comparing Z3 and SymPy solver.
 """
 import pytest
+import sympy.core.power
 
 from sympy import symbols, Poly, PolynomialError, Interval, oo
 from sympy.solvers.inequalities import solve_rational_inequalities
-from z3 import *
+from typing import List, Tuple
+from z3 import Real, Solver, Ints, sat
 
 
-def solve_inequalities_sympy(ineq_list: list) -> None:
+def solve_inequalities_sympy(ineq_list: List[Tuple[sympy.core.Expr, sympy.core.Expr, str]]):
     """
     SymPy's support for inequalities are a little hard to read. This is just a wrapper for inequalities.
-    l is expected to be a list of (LHS, VARS, OP) where the inequality is "LHS OP 0", VARS contains all
+    `ineq_list` is expected to be a list of (LHS, VARS, OP) where the inequality is "LHS OP 0", VARS contains all
     variables used in the inequality. So for example, (x + 1, x, >) represents "x + 1 > 0"
     """
     inequalities = []
@@ -20,7 +22,7 @@ def solve_inequalities_sympy(ineq_list: list) -> None:
     return solve_rational_inequalities([inequalities])
 
 
-def test_compare_z3_and_sympy_solver() -> None:
+def test_compare_z3_and_sympy_solver():
     """
     A very simple inequality case which both Z3 and SymPy can solve. Note that "solve" in Z3's context means
     differently from that in SymPy's. The latter is trying to solve a harder problem.
@@ -41,10 +43,10 @@ def test_compare_z3_and_sympy_solver() -> None:
     assert solve_inequalities_sympy([(x**2-1, x, ">"), (x, x, "<")]) == Interval.open(-oo, -1)
 
 
-def test_compare_z3_and_sympy_solver_sympy_fail() -> None:
+def test_compare_z3_and_sympy_solver_sympy_fail():
     """
     SymPy's solve() targets a harder goal. So the scope of its solvable problems are smaller.
-    It fails some simple tasks solvable in Z3. For example, it doesn't support >1 variables inequalities.
+    It fails some simple tasks solvable in Z3. For example, it doesn't support inequalities with multiple variables.
     """
     x, y = Ints('x y')
     s = Solver()
@@ -54,7 +56,7 @@ def test_compare_z3_and_sympy_solver_sympy_fail() -> None:
     y0: int = s.model()[y].as_long()
     assert x0 > 2 and y0 < 10 and x0 + 2 * y0 == 7
 
-    # SymPy's solver is ill-equipped to solve the set of inequality.
+    # SymPy's solver is ill-equipped to solve the set of inequalities.
     x, y = symbols('x y')
     with pytest.raises(PolynomialError) as exc_info:
         solve_inequalities_sympy([(x ** 2 - 1, x, ">"), (y - 10, y, "<"), (x + 2 * y - 7, (x, y), "==")])
